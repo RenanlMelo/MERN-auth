2cf48929c8a8149c5acfb79403cefadb04322ce4
RenanlMelo <renanlaramelo@gmail.com>
2023-12-27 20:21:31 -0300

    create api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,15 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
 
     <BrowserRouter>
+      {/* header */}
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,5 +23,5 @@ export default function App() {
 
     </BrowserRouter>
   
-  ) 
+  ); 
 }
