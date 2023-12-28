8a3c689bb0827d885510f2d01e657cc136f7a734
RenanlMelo <renanlaramelo@gmail.com>
2023-12-28 12:24:25 -0300

    create sign up api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       {/* header */}
       <Header />
 
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
