5ab8b6a92a0cc87857d9455aee1a0282e406b155
RenanlMelo <renanlaramelo@gmail.com>
2023-12-28 22:16:02 -0300

    complete sign up page functIonality

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
       boxShadow: {
         '5xl': '20px 20px 50px rgba(0, 0, 0, 0.5)',
       },
+
+      dropShadow: {
+        '4xl': '0 45px 65px rgba(0, 0, 0, 0.15)',
+      },
       
       keyframes: {
 
@@ -17,10 +21,26 @@ module.exports = {
          '100%': { transform: 'scale(0.9)', opacity: '.9' },
           },
 
+        blob: {
+          '0%': {
+            transform: 'scale(1)'
+          },
+          '33%': {
+            transform: 'scale(1.2)'
+          },
+          '66%': {
+            transform: 'scale(0.8)'
+          },
+          '100%': {
+            transform: 'scale(1)'
+          },
+        }
+
         },
 
       animation: {
         'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'blob': 'blob 7s infinite',
       },
       
     }, //extend
